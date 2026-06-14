d1c2a87f2f3b154019afd3e019de708515be8e42
Hadi <hadimosawi782@gmail.com>
2026-06-14 22:40:37 +0430

    fix database fallback

--- config.py
+++ config.py
@@ -1,28 +1,23 @@
 ﻿import os
 import sys
 
-# تشخیص Render
-IS_RENDER = os.environ.get("RENDER") == "true"
-
-# دیتابیس آنلاین
-if IS_RENDER:
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace(
-            "postgres://",
-            "postgresql://",
-            1
-        )
-
-# دیتابیس آفلاین
+# مسیر دیتابیس
+if getattr(sys, 'frozen', False):
+    BASE_DIR = os.path.dirname(sys.executable)
 else:
-    if getattr(sys, 'frozen', False):
-        BASE_DIR = os.path.dirname(sys.executable)
-    else:
-        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'supermarket.db')}"
+LOCAL_DB = f"sqlite:///{os.path.join(BASE_DIR, 'supermarket.db')}"
+
+# اگر DATABASE_URL وجود داشت از آن استفاده کن
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
 class Config:
     SECRET_KEY = os.environ.get(
@@ -30,5 +25,6 @@ class Config:
         "YOUR-SECRET"
     )
 
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or LOCAL_DB
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
